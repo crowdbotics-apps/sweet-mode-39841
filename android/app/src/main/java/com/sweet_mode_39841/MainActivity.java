e05d00ea19b403b18d0a990264030b7b770df0d3
Crowdbotics <team@crowdbotics.com>
2023-04-11 21:05:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sweet_mode_39841/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sweet_mode_39841;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "sweet_mode_39841";
+  }
+}
